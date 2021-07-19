4164454ce3cb2cbf6cc8183e8e048a61a6f55519
unknown <2603029264@qq.com>
2021-07-19 17:46:04 +0800

    第三次修改

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -8,32 +8,32 @@ import LeftNav from '../../components/LeftNav';
 import Home from '../Home';
 import Category from '../Category';
 import Article from '../Article';
-import Operat from '../Article/Operat';
+import Operat from '../Operat';
 import User from '../User';
 const { Footer, Sider, Content } = Layout;
 
 export default class Admin extends Component {
-	render() {
-		return (
-			<Layout style={{ height: '100%' }}>
-				<Sider>
-					<LeftNav />
-				</Sider>
-				<Layout>
-					<Header>Header</Header>
-					<Content style={{ backgroundColor: '#ffffff' }}>
-						<Switch>
-							<Route path="/home" component={Home}></Route>
-							<Route path="/category" component={Category}></Route>
-							<Route path="/article" component={Article}></Route>
-							<Route path="/operat" component={Operat}></Route>
-							<Route path="/user" component={User}></Route>
-							<Redirect to="/home" />
-						</Switch>
-					</Content>
-					<Footer style={{ textAlign: 'center', color: '#ccc' }}>值得信赖的小徐</Footer>
-				</Layout>
-			</Layout>
-		);
-	}
+  render() {
+    return (
+      <Layout style={{ height: '100%' }}>
+        <Sider>
+          <LeftNav />
+        </Sider>
+        <Layout>
+          <Header>Header</Header>
+          <Content style={{ backgroundColor: '#ffffff' }}>
+            <Switch>
+              <Route path="/home" component={Home}></Route>
+              <Route path="/category" component={Category}></Route>
+              <Route path="/article" component={Article}></Route>
+              <Route path="/operat" component={Operat}></Route>
+              <Route path="/user" component={User}></Route>
+              <Redirect to="/home" />
+            </Switch>
+          </Content>
+          <Footer style={{ textAlign: 'center', color: '#ccc' }}>值得信赖的小徐</Footer>
+        </Layout>
+      </Layout>
+    );
+  }
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,55 @@
 import React, { Component } from 'react';
+import './index.less';
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 export default class Login extends Component {
-	render() {
-		return <div>Login</div>;
-	}
+  render() {
+    return (
+      <div className="login">
+        <div>
+          <div className="title">
+            <h1>小徐博客管理系统</h1>
+          </div>
+          <div className="login-form">
+            <Form
+              name="normal_login"
+              className="login-form"
+              initialValues={{
+                remember: true
+              }}
+            >
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: '请输入用户名!'
+                  }
+                ]}
+              >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: '请输入密码！'
+                  }
+                ]}
+              >
+                <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="密码" />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                  Log in
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
